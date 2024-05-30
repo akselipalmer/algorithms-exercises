f7ad82e1fcffcd46ca43c12769595d79b7a5a36f
Akseli Palmer <akseli@toddsden.com>
2024-05-30 17:01:19 -0300

    did bubble sort using a do while loop

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,26 +10,21 @@
   index's. Continue looping through until all values are in ascending order
 */
 
-function swapNumbers(index, array, changed) {
-  if (array[index + 1] !== undefined) {
-    const newArray = [...array];
-    const newIndex = index + 1;
-    if (array[index] > array[index + 1]) {
-      newArray[index] = array[index + 1];
-      newArray[index + 1] = array[index];
-      return swapNumbers(newIndex, newArray, true);
-    }
-    return swapNumbers(newIndex, newArray, changed);
-  }
-  if (changed) {
-    return swapNumbers(0, array, false);
-  }
-  return array;
-}
-
 function bubbleSort(nums) {
-  const changedArray = swapNumbers(0, array, false);
-  return changedArray;
+  let changed = false;
+  do {
+    changed = false;
+    for (let i = 0; i < nums.length; i++) {
+      const currentValue = nums[i];
+      const nextValue = nums[i + 1];
+      if (currentValue > nextValue) {
+        nums[i] = nextValue;
+        nums[i + 1] = currentValue;
+        changed = true;
+      }
+    }
+  } while (changed);
+  return nums;
 }
 
 // unit tests
