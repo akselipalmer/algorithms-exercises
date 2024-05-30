1917b573891aebde54ce00abdcb29736b66d3f56
Akseli Palmer <akseli@toddsden.com>
2024-05-30 16:24:22 -0300

    completed bubble sort exersize

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,8 +10,26 @@
   index's. Continue looping through until all values are in ascending order
 */
 
+function swapNumbers(index, array, changed) {
+  if (array[index + 1] !== undefined) {
+    const newArray = [...array];
+    const newIndex = index + 1;
+    if (array[index] > array[index + 1]) {
+      newArray[index] = array[index + 1];
+      newArray[index + 1] = array[index];
+      return swapNumbers(newIndex, newArray, true);
+    }
+    return swapNumbers(newIndex, newArray, changed);
+  }
+  if (changed) {
+    return swapNumbers(0, array, false);
+  }
+  return array;
+}
+
 function bubbleSort(nums) {
-  // code goes here
+  const changedArray = swapNumbers(0, array, false);
+  return changedArray;
 }
 
 // unit tests
