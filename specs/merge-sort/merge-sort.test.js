0723ef9734b63876d328d0f729dea52d9ceca99b
Akseli Palmer <akseli@toddsden.com>
2024-06-05 17:15:52 -0300

    finished my version of merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,51 @@
   You'll need to write more than just one function
 */
 
+function merge(arrayOne, arrayTwo) {
+  const newArray = [];
+  do {
+    if (
+      (arrayTwo.length === 0 && arrayOne.length > 0) ||
+      arrayOne[0] < arrayTwo[0]
+    ) {
+      newArray.push(arrayOne[0]);
+      arrayOne.shift();
+    } else {
+      newArray.push(arrayTwo[0]);
+      arrayTwo.shift();
+    }
+  } while (arrayOne.length > 0 || arrayTwo.length > 0);
+
+  return newArray;
+}
+
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length === 1) {
+    // return sorted list of length 1
+    return nums;
+  } else {
+    // array need to be split again
+    // split and call mergeSort twice with new split arrays
+    let midpoint = Math.floor(nums.length / 2);
+    let firstHalf;
+    let secondHalf;
+
+    if (nums.length % 2 === 0) {
+      // If the array length is even
+      firstHalf = nums.slice(0, midpoint);
+      secondHalf = nums.slice(midpoint);
+    } else {
+      // If the array length is odd
+      firstHalf = nums.slice(0, midpoint + 1);
+      secondHalf = nums.slice(midpoint + 1);
+    }
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
